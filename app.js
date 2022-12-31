cb2317e8cbe9e76e68dfc52c59d9f26360022300
Sohag Himon <sohagdev19@gmail.com>
2022-12-31 17:15:10 +0600

    second commit

--- app.js
+++ app.js
@@ -1,116 +1,141 @@
-const menuItems = [
-  {
-    name: 'French Fries with Ketchup',
-    price: 223,
-    image: 'images/plate__french-fries.png',
-    alt: 'French Fries',
-    count: 0
-  },
-  {
-    name: 'Salmon and Vegetables',
-    price: 512,
-    image: 'images/plate__salmon-vegetables.png',
-    alt: 'Salmon and Vegetables',
-    count: 0
-  },
-  {
-    name: 'Spaghetti Meat Sauce',
-    price: 782,
-    image: 'images/plate__spaghetti-meat-sauce.png',
-    alt: 'Spaghetti with Meat Sauce',
-    count: 0
-  },
-  {
-    name: 'Bacon, Eggs, and Toast',
-    price: 599,
-    image: 'images/plate__bacon-eggs.png',
-    alt: 'Bacon, Eggs, and Toast',
-    count: 0
-  },
-  {
-    name: 'Chicken Salad with Parmesan',
-    price: 698,
-    image: 'images/plate__chicken-salad.png',
-    alt: 'Chicken Salad with Parmesan',
-    count: 0
-  },
-  {
-    name: 'Fish Sticks and Fries',
-    price: 634,
-    image: 'images/plate__fish-sticks-fries.png',
-    alt: 'Fish Sticks and Fries',
-    count: 0
-  }
-]
-// Show and hide the cart using the add to cart button
+// Selecting all the elements
+const menuEl = document.querySelector('.menu ul')
+const cartEl = document.querySelector('.cart-summary')
+const emptyEl = document.querySelector('.empty')
+const totalEl = document.querySelector('.totals')
+let cart = []
 
-const addCartBtn = document.querySelectorAll('.add')
-const cartSummerySection = document.querySelector('.cart-summary')
-const empty = document.querySelector('.empty')
-let cartItems = []
-
-for (let i = 0; i < addCartBtn.length; i++) {
-  addCartBtn[i].addEventListener('click', () => {
-    addCartBtn[i].classList.remove('add')
-    addCartBtn[i].classList.add('in-cart')
-    addCartBtn[i].innerHTML = `<img src="images/check.svg" alt="Check" />
-    In Cart`
-    addCartBtn[i].disabled = true
-    empty.style.display = 'none'
-    addToCart(i)
+// Rendering all the menu item
+const renderMenuItems = () => {
+  menuItems.forEach((item) => {
+    menuEl.innerHTML += `
+    <li>
+    <div class="plate">
+      <img
+        src="${item.image}"
+        alt="${item.alt}"
+        class="plate"
+      />
+    </div>
+    <div class="content">
+      <p class="menu-item">${item.name}</p>
+      <p class="price">$${item.price}</p>
+      <button data-id="${item.id}" class="add" onClick="addToCart(${item.id})" >Add to Cart</button>
+    </div>
+  </li>
+    `
   })
 }
 
-// add to cart functionality
-const addToCart = (i) => {
-  for (let n = 0; n < menuItems.length; n++) {
-    const selectedMenuItem = menuItems[n]
-    if (i === n) {
-      cartItems.push(selectedMenuItem)
-      const div = document.createElement('div')
-      div.innerHTML = `
-         <li>
-            <div class="plate">
-              <img
-                 src='${selectedMenuItem.image}'
-                 alt="Fish Sticks and Fries"
-                class="plate"
+renderMenuItems()
+
+// Add the selected items to the cart
+const addToCart = (id) => {
+  if (cart.some((item) => item.id === id)) {
+    alert('this item is already in the cart!')
+  } else {
+    const item = menuItems.find((item) => item.id === id)
+    cart.push({
+      ...item,
+      count: 1
+    })
+    console.log(cart)
+  }
+  updateCart()
+}
+
+const updateCart = () => {
+  emptyEl.style.display = 'none'
+  renderCart()
+  renderSubtotal()
+}
+
+const renderCart = () => {
+  cartEl.innerHTML = '' // clear the cart element before adding the new one
+  cart.forEach((item) => {
+    // totalPriceOfCartItem = item.price * item.count
+    cartEl.innerHTML += `
+    <li id="cart-id">
+          <div class="plate">
+             <img
+                src="${item.image}"
+                alt="Fish Sticks and Fries"
+               class="plate"
               />
-              <div class="quantity">${selectedMenuItem.count + 1}</div>
-            </div>
+              <div class="quantity" onClick="removeItemFromCart(${item.id})">
+              &#10006
+              </div>
+          </div>
             <div class="content">
-              <p class="menu-item">${selectedMenuItem.name}</p>
-               <p class="price">$${selectedMenuItem.price}</p>
-           </div>
-             <div class="quantity__wrapper">
-               <button class="decrease">
+                  <p class="menu-item">${item.name}</p>
+                  <p class="price">$${item.price}</p>
+              </div>
+         <div class="quantity__wrapper">
+            <button class="decrease" onClick="changeNumberOfUnits('minus', ${
+              item.id
+            })">
+           <img src="images/chevron.svg" />
+              </button>
+              <div id="quantity" class="quantity">${item.count}</div>
+              <button class="increase" onClick="changeNumberOfUnits('plus', ${
+                item.id
+              })">
                  <img src="images/chevron.svg" />
-              </button>
-              <div class="quantity">1</div>
-              <button class="increase">
-                <img src="images/chevron.svg" />
-              </button>
-            </div>
-            <div class="subtotal">$${selectedMenuItem.price}</div>
-         </li>
-      `
-      cartSummerySection.appendChild(div)
-      increaseCountFunc(menuItems[n])
+               </button>
+             </div>
+             <div class="subtotal">$${item.price * item.count}</div>
+       </li>
+    `
+  })
+}
+// remove the item from the cart
+
+const removeItemFromCart = (id) => {
+  cart = cart.filter((item) => item.id !== id)
+  updateCart()
+}
+
+// rendering the subtotal
+const renderSubtotal = () => {
+  let totalPrice = 0,
+    taxRate = 0,
+    finalPrice = 0
+  cart.forEach((item) => {
+    totalPrice += item.count * item.price
+    taxRate += item.price / 10
+    finalPrice += totalPrice + taxRate
+    totalEl.innerHTML = `
+          <div class="line-item">
+              <div class="label">Subtotal:</div>
+              <div class="amount price subtotal">$${totalPrice.toFixed(2)}</div>
+          </div>
+          <div class="line-item">
+              <div class="label">Tax:</div>
+              <div class="amount price tax">$${taxRate.toFixed(2)}</div>
+          </div>
+          <div class="line-item total">
+              <div class="label">Total:</div>
+              <div class="amount price total">$${finalPrice.toFixed(2)}</div>
+          </div>
+    `
+  })
+}
+
+// Change the the Number of Units
+const changeNumberOfUnits = (action, id) => {
+  cart = cart.map((item) => {
+    let numberOfUnits = item.count
+    if (item.id === id) {
+      if (action === 'plus') {
+        numberOfUnits++
+      } else if (action === 'minus' && numberOfUnits > 0) {
+        numberOfUnits--
+      }
     }
-  }
+    return {
+      ...item,
+      count: numberOfUnits
+    }
+  })
+  updateCart()
 }
-function increaseCountFunc(menuItem) {
-  let quantity = 0
-  quantity++
-  menuItem.count = quantity
-}
-
-// increasing & decreasing the quantity
-document.addEventListener('click', (e) => {
-  const increaseBtn = e.target.closest('.increase')
-  const decreaseBtn = e.target.closest('.decrease')
-
-  if (increaseBtn) {
-    itemIncreased()
-  }
-})
